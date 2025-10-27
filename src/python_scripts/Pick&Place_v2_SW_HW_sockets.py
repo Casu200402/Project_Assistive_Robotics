3d20e03f855bf437d0564bb9bf0f4ca0d4e3eb91
Alesco8 <alejandrojcs@hotmail.com>
2025-10-27 11:54:13 +0100

    Constants del robot per scripts i init

--- src/python_scripts/Pick&Place_v2_SW_HW_sockets.py
+++ src/python_scripts/Pick&Place_v2_SW_HW_sockets.py
@@ -40,10 +40,57 @@ robot.setPoseFrame(base)
 robot.setPoseTool(tool)
 robot.setSpeed(80)
 
+# Robot Constants
+ROBOT_IP = '192.168.1.5'
+ROBOT_PORT = 30002
+accel_mss = 1.2
+speed_ms = 0.75
+blend_r = 0.0
+timej = 6
+timel = 4
+
+# URScript commands
+set_tcp = "set_tcp(p[0.000000, 0.000000, 0.050000, 0.000000, 0.000000, 0.000000])"
+movel_init = f"movel([-1.009423, -1.141297, -1.870417, 3.011723, -1.009423, 0.000000],1.20000,0.75000,{timel},0.0000)"
+movel_app_shake = f"movel([-2.268404, -1.482966, -2.153143, -2.647089, -2.268404, 0.000000],{accel_mss},{speed_ms},{timel},0.000)"
+movel_shake = f"movel([-2.268404, -1.663850, -2.294637, -2.324691, -2.268404, 0.000000],{accel_mss},{speed_ms},{timel/2},0.000)"
+movel_app_give5 = f"movel([-2.280779, -1.556743, -2.129529, 5.257071, -1.570796, 2.280779],{accel_mss},{speed_ms},{timel},0.000)"
+movel_give5 = f"movel([-2.195869, -1.642206, -2.040971, 5.253965, -1.570796, 2.195869],{accel_mss},{speed_ms},{timel/2},0.000)"
+
+# Check robot connection
+def check_robot_port(ip, port):
+    global robot_socket
+    try:
+        robot_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        robot_socket.settimeout(1)
+        robot_socket.connect((ip, port))
+        return True
+    except (socket.timeout, ConnectionRefusedError):
+        return False
+# Send URScript command
+def send_ur_script(command):
+    robot_socket.send((command + "\n").encode())
+# Wait for robot response
+def receive_response(t):
+    try:
+        print("Waiting time:", t)
+        time.sleep(t)
+    except socket.error as e:
+        print(f"Error receiving data: {e}")
+        exit(1)
+
 def Init():
     print("Init")
     robot.MoveL(Init_target, True)
     print("Init_target REACHED")
+    if robot_is_connected:
+        print("Init REAL UR5e")
+        send_ur_script(set_tcp)
+        receive_response(1)
+        send_ur_script(movel_init)
+        receive_response(timel)
+    else:
+        print("UR5e not connected. Simulation only.")
 
 def Pick():
     print("Pick")
@@ -66,9 +113,13 @@ def Place():
 
 # Main function
 def main():
+    global robot_is_connected
+    robot_is_connected = check_robot_port(ROBOT_IP, ROBOT_PORT)
     Init()
     Pick()
     Place()
+    if robot_is_connected:
+        robot_socket.close()
 
 if __name__ == "__main__":
     main()
